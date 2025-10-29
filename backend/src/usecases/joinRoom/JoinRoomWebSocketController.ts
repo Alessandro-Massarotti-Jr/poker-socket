06f63cd1c37fcd3afbd4de17b630cc2fbe50d67e
Alessandro-Massarotti-junior <alemassajr@hotmail.com>
2025-10-29 17:01:21 -0300

    fix(backend): fix event name

--- backend/src/usecases/joinRoom/JoinRoomWebSocketController.ts
+++ backend/src/usecases/joinRoom/JoinRoomWebSocketController.ts
@@ -23,7 +23,7 @@ export class JoinRoomWebSocketController {
       participant: { id: socket.id, name: validation.data.participantName },
     });
     await socket.join(room.id);
-    socket.emit("room:Joined", { room: room });
-    socket.to(room.id).emit("room:Joined", { room: room });
+    socket.emit("room:joined", { room: room });
+    socket.to(room.id).emit("room:joined", { room: room });
   }
 }
